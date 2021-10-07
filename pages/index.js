23a3434e339d9180a47008b0a477a2f567124b57
Seth Barton <sethwbarton@gmail.com>
2021-10-07 15:33:30 -0600

    It looks a little better. I'm not styling those buttons though.

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
 import Head from 'next/head'
 import React from "react";
 import {useState, useEffect} from "react";
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import memes from '/public/memes.json'
+import styled from "styled-components";
 
 export default function Home() {
     const [currentMeme, setCurrentMeme]= useState(0)
@@ -16,30 +16,46 @@ export default function Home() {
           </Head>
 
           <main className={styles.main}>
-            <div>
+            <div style={{marginBottom: "50px"}}>
                 {/* eslint-disable-next-line react/no-unescaped-entities */}
-                You've got {memes.memes.length} to read.
+                You've got {memes.memes.length} memes to view!
             </div>
-              <img src={memes.memes[currentMeme].resource}/>
-              <button onClick={() => {
-                  if (currentMeme < memes.memes.length - 1) {
-                      setCurrentMeme(currentMeme + 1)
-                  }
-              }}>
-                  Next
-              </button>
-              <button onClick={() => {setCurrentMeme(Math.floor(Math.random() * memes.memes.length))}}>
-                  {/* eslint-disable-next-line react/no-unescaped-entities */}
-                  I'm Feeling Lucky
-              </button>
-              <button onClick={() => {
-                  if (currentMeme > 0) {
-                      setCurrentMeme(currentMeme - 1)
-                  }
-              }}>
-                  Previous
-              </button>
+              <Meme src={memes.memes[currentMeme].resource}/>
+
+              <ButtonsContainer>
+                  <button onClick={() => {
+                      if (currentMeme < memes.memes.length - 1) {
+                          setCurrentMeme(currentMeme + 1)
+                      }
+                  }}>
+                      Next
+                  </button>
+                  <button onClick={() => {setCurrentMeme(Math.floor(Math.random() * memes.memes.length))}}>
+                      {/* eslint-disable-next-line react/no-unescaped-entities */}
+                      I'm Feeling Lucky
+                  </button>
+                  <button onClick={() => {
+                      if (currentMeme > 0) {
+                          setCurrentMeme(currentMeme - 1)
+                      }
+                  }}>
+                      Previous
+                  </button>
+              </ButtonsContainer>
           </main>
         </div>
     )
 }
+
+
+const Meme = styled.img`
+  width: 100%;
+  height: 500px;
+`
+
+const ButtonsContainer = styled.div`
+  display: flex;
+  flex-direction: row-reverse;
+  margin-top: 50px;
+  gap: 20px;
+`
